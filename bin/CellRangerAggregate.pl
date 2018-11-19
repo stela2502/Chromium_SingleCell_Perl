6c604286d42f80406eb2da20b8a8639c8d101f21
stefanlang <Stefan.Lang@med.lu.se>
2018-11-19 09:18:55 +0100

    Added a simple CellRangerAggregate_Seurat.R scriqpt

--- bin/CellRangerAggregate.pl
+++ bin/CellRangerAggregate.pl
@@ -25,11 +25,11 @@
 =head1  SYNOPSIS
 
     CellRangerAggregate.pl
-       -path       :<please add some info!>
-       -options     :<please add some info!> you can specify more entries to that
-                         format: key_1 value_1 key_2 value_2 ... key_n value_n
-       -outpath       :<please add some info!>
-
+       -path        :search this path for all molecule_info.h5 files and merge them
+       -options     :the SLURM options 
+                     e.g "A 'lsens2018-3-3' t 12:00:00 N 1 n 15 p dell "
+       -outpath     :where to create the summary data
+       -summary_ID  :the new sample name
 
        -help           :print this help
        -debug          :verbose output
@@ -115,7 +115,7 @@ $task_description .= 'perl '.$plugin_path .'/CellRangerAggregate.pl';
 $task_description .= " -path '$path'" if (defined $path);
 $task_description .= ' -options "'.join( '" "', @options ).'"' if ( defined $options[0]);
 $task_description .= " -outpath '$outpath'" if (defined $outpath);
-
+$task_description .= " -summary_ID '$summary_ID'" if ( defined $summary_ID);
 
 for ( my $i = 0 ; $i < @options ; $i += 2 ) {
 	$options[ $i + 1 ] =~ s/\n/ /g;
@@ -161,7 +161,7 @@ print LOG "#CellRanger cmd:\t$cmd\n";
 my $runner = "runCommand.pl -options ".join(" ", @options )
 . " -loadModules 'GCCcore/6.3.0' 'bcl2fastq/2.19.1' 'cellranger/2.2.0'"
 . " -cmd '$cmd'"
-. " -outfile $path/CellRangerAggregate"
+. " -outfile $outpath/CellRangerAggregate"
 ;
 if ( $debug ) {
 	$runner .= " -debug";
@@ -174,4 +174,108 @@ close ( LOG );
 print $runner."\n";
 
 system( $runner );
+
+## here I need to find the correct file
+
+open ( IN ,"find $path -name 'genes.tsv' |") or die "$!\n";
+## I am really only after the genome info.
+my $genome;
+while( <IN> ) {
+	@tmp = split("/", $_ );
+	$genome = $tmp[-2];
+	last;
+}
+close ( IN );
+
+open ( OUT, ">$outpath/CellRangerAggregate_Seurat.R" ) or die "I can not create '$outpath/CellRangerAggregate_Seurat.R'\n$!\n";
+
+print OUT "
+library(Seurat)
+data <- Read10X(data.dir = './$summary_ID/outs/filtered_gene_bc_matrices_mex/$genome/')
+pbmc <- CreateSeuratObject(raw.data = data,
+                           min.cells = 3,
+                           min.genes = 200,
+                           project = '$summary_ID')
+
+mito.genes <- grep(pattern = '^[Mm][Tt]-', x = rownames(x = pbmc\@data), value = TRUE)
+                           
+if ( length(mito.genes) > 0 ){
+
+percent.mito <- Matrix::colSums(pbmc\@raw.data[mito.genes, ]) / Matrix::colSums(pbmc\@raw.data)
+
+## filter high mitocondrial
+pbmc <- FilterCells(object = pbmc,
+                    subset.names = c('nGene', 'percent.mito'),
+                    low.thresholds = c(200, -Inf),
+                    high.thresholds = c(2500, 0.05))
+}else {
+	pbmc <- FilterCells(object = pbmc,
+                    subset.names = c('nGene'),
+                    low.thresholds = c(200),
+                    high.thresholds = c(2500))
+}
+
+pbmc <- NormalizeData(object = pbmc,
+                      normalization.method = 'LogNormalize',
+                      scale.factor = 1e4)
+
+pbmc <- FindVariableGenes(object = pbmc,
+                          mean.function = ExpMean,
+                          dispersion.function = LogVMR,
+                          x.low.cutoff = 0.0125,
+                          x.high.cutoff = 3,
+                          y.cutoff = 0.5)
+if ( length(mito.genes) > 0 ){
+pbmc <- ScaleData(object = pbmc,
+                  vars.to.regress = c('nUMI', 'percent.mito'))
+}else {
+	pbmc <- ScaleData(object = pbmc,
+                  vars.to.regress = c('nUMI'))
+}
+pbmc <- RunPCA(object = pbmc,
+               pc.genes = pbmc\@var.genes,
+               do.print = TRUE,
+               pcs.print = 1:5,
+               genes.print = 5)
+
+pbmc <- RunTSNE(object = pbmc,
+                dims.use = 1:10,
+                do.fast = TRUE)
+
+pbmc <- FindClusters(object = pbmc, reduction.type = 'TSNE', dims.use = 1:10, 
+    resolution = 0.6, print.output = 0, save.SNN = TRUE)
+
+cluster1.markers <- FindMarkers(object = pbmc,
+                                ident.1 = 1,
+                                min.pct = 0.25)
+
+t = read.delim( 'Aggregation.csv', sep=',')
+
+snameID = unlist(lapply(str_split( names(pbmc\@ident), '-'), function(x) { x[2]}))
+
+sname = factor(as.vector(t[,1])[as.numeric(snameID)], levels= as.vector(t[,1]) )
+
+pbmc = SetIdent(pbmc, ident.use= sname, cells.use=pbmc\@ident )
+
+pbmc <- FindClusters(object = pbmc, reduction.type = 'pca', dims.use = 1:10, 
+    resolution = 0.6, print.output = 0, save.SNN = TRUE)
+
+x11(type='cairo')
+
+TSNEPlot(object = pbmc, do.label = TRUE, pt.size = 0.5)
+
+saveRDS(pbmc, file='Seurat_$summary_ID.RData')
+
+cluster1.markers <- FindMarkers(object = pbmc, ident.1 = 1, min.pct = 0.25)
+
+all.markers =  FindAllMarkers(object = pbmc, min.pct = 0.25)
+
+
+q(save = 'yes')
+
+";
+
+close ( OUT );
+
+print "When everything is finished you can use the R script $outpath/CellRangerAggregate_Seurat.R as a first analysis\n";
 
